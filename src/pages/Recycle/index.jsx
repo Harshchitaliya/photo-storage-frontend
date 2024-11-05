da9b8275924831600dbd38e8fdb6c06f373b84d2
Harshchitaliya <chitaliyaharsh09@gmail.com>
2024-11-05 13:37:47 +0530

    fix code

--- src/pages/Recycle/index.jsx
+++ src/pages/Recycle/index.jsx
@@ -6,7 +6,7 @@ import { DeleteIcon, RecycleIcon } from "../../components/Icons";
 import { Button, Checkbox, Toast } from "flowbite-react";
 import Loader from "../../components/Loader";
 import SearchInput from "../../components/SearchInput";
-import { showRecycle, permentDelete } from "../../server";
+import { setAllPhoto, permentDelete, deletePhoto } from "../../server";
 
 const Recycle = () => {
     const [photo, setPhoto] = useState([]);
@@ -15,31 +15,35 @@ const Recycle = () => {
     const [search, setSearch] = useState("");
     const [filteredPhoto, setFilteredPhoto] = useState([]);
     const { currentUseruid } = useAuth();
+    const galleryphoto = false;
+    const isrecycle = true;
     useEffect(() => {
         let filtered = photo;
         if (search) {
             filtered = photo.filter((item) => item.allSearch.includes(search));
         }
-      
+
         setFilteredPhoto(filtered);
     }, [search, photo]);
 
- 
+
 
     useEffect(() => {
         handleShowPhoto();
     }, []);
 
     const handleShowPhoto = async () => {
-        if(photo.length < 0){
+        if (photo.length < 0) {
             setLoading(true);
         }
 
         try {
-            const allphotos = await showRecycle({
+            const allphotos = await setAllPhoto({
                 currentUseruid,
                 firestore,
                 storage,
+                galleryphoto
+
             });
             setPhoto(allphotos);
         } catch (error) {
@@ -83,78 +87,83 @@ const Recycle = () => {
         }
     };
 
-    const handleDelete = async (url) => {
-        const urls = url;
+    const handleDelete = async (urls) => {
         console.log(currentUseruid);
-        try{
+        try {
+            setLoading(true);
             await permentDelete({ urls, currentUseruid, firestore, storage })
             handleShowPhoto();
             setSelectedItems([]);
-        }catch(error){
+        } catch (error) {
             console.error("Error deleting photos:", error);
+        } finally {
+            setLoading(false);
         }
 
     }
 
     const handleRecycle = async (urls) => {
-        
-    }
+        try {
+            setLoading(true);
+            await deletePhoto({ urls, currentUseruid, firestore, isrecycle })
+            handleShowPhoto();
+            setSelectedItems([]);
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false);
+        }
 
+    }
     return (
         <div>
-
             <div className="flex items-center justify-between">
                 <div className="flex items-center p-2">
                     <Checkbox
+                        className="cursor-pointer"
                         checked={selectedItems.length === filteredPhoto.length && filteredPhoto.length > 0}
                         onChange={handleSelectAll}
                     />
-                    <label className="ml-2 text-sm text-gray-500">Select All</label>
+                    <label className="ml-2 text-sm text-gray-500 cursor-pointer" onClick={handleSelectAll}>Select All</label>
                 </div>
-             
+
                 <SearchInput onSearch={setSearch} />
             </div>
-
-
             <div
                 className="flex flex-wrap justify-center items-center gap-6 mt-3 overflow-y-auto"
                 style={{ height: "calc(100vh - 120px)" }}
             >
-                {loading ? (
-                    <div className="flex justify-center items-center h-screen">
-                        <Loader />
-                    </div>
-                ) : null}
-                {filteredPhoto.map((photoUrl, index) => (
-                    <ProductCard
-                        photoUrl={photoUrl}
-                        key={index}
-                        checkboxClick={setSelectedItems}
-                        checked={selectedItems}
-                        handleDelete={handleDelete}
-                        handleRecycle={handleRecycle}
-                    />
-                ))}
+                {filteredPhoto.length > 0 ? (
+                    filteredPhoto.map((photoUrl, index) => (
+                        <ProductCard
+                            photoUrl={photoUrl}
+                            key={index}
+                            checkboxClick={setSelectedItems}
+                            checked={selectedItems}
+                            handleDelete={handleDelete}
+                            handleRecycle={handleRecycle}
+                        />
+                    ))
+                ) : (
+                    <p>No photos available</p>
+                )}
             </div>
-
             {selectedItems.length > 0 && (
                 <div className="fixed bottom-4 right-4">
                     <Toast>
                         <div className="flex items-center gap-4">
                             <span>{selectedItems.length} selected</span>
-                          
+
                             <Button onClick={() => handleDelete(selectedItems)}>
                                 <DeleteIcon />
                             </Button>
                             <Button onClick={() => handleRecycle(selectedItems)}>
                                 <RecycleIcon />
                             </Button>
-
                         </div>
                     </Toast>
                 </div>
             )}
-            
         </div>
     );
 };
--- src/server/photo.jsx
+++ src/server/photo.jsx
@@ -6,7 +6,7 @@ import { deleteObject } from "firebase/storage";
 import { isVideo } from "../utils";
 
 export const setAllPhoto = async (props) => {
-  const { currentUseruid, firestore, storage } = props;
+  const { currentUseruid, firestore, storage, galleryphoto } = props;
   const userDocRef = doc(firestore, "Users", currentUseruid);
   const userDoc = await getDoc(userDocRef);
   if (userDoc.exists()) {
@@ -14,8 +14,14 @@ export const setAllPhoto = async (props) => {
     const photoData = userData.skus
       .flatMap((sku) =>
         sku.photos.map((photo) => {
-          if (photo.isDeleted) {
-            return null;
+          if (galleryphoto) {
+            if (photo.isDeleted) {
+              return null;
+            }
+          } else {
+            if (!photo.isDeleted) {
+              return null;
+            }
           }
           return {
             url: photo.url,
@@ -50,7 +56,7 @@ export const setAllPhoto = async (props) => {
 };
 
 export const deletePhoto = async (props) => {
-  const { urls, currentUseruid, firestore, handleShowPhoto, setSelectedItems, } = props;
+  const { urls, currentUseruid, firestore, handleShowPhoto, setSelectedItems,isrecycle } = props;
   if (!urls || urls.length === 0) return;
 
   const userDocRef = doc(firestore, "Users", currentUseruid);
@@ -62,15 +68,12 @@ export const deletePhoto = async (props) => {
         ...sku,
         photos: sku.photos.map((photo) => ({
           ...photo,
-          isDeleted: urls.includes(photo.url) ? true : photo.isDeleted,
+          isDeleted: isrecycle?(urls.includes(photo.url) ? false : photo.isDeleted):(urls.includes(photo.url) ? true : photo.isDeleted)
         })),
       }));
       await updateDoc(userDocRef, {
         skus: updatedSkus,
-      }).then(() => {
-        handleShowPhoto();
-        setSelectedItems([]);
-      });
+      })
     }
   } catch (error) {
     console.error("Error deleting photos:", error);
@@ -107,68 +110,39 @@ export const setFavorite = async (props) => {
 }
 
 
-export const showRecycle = async (props) => {
-  const { currentUseruid, firestore, storage } = props;
-  const userDocRef = doc(firestore, "Users", currentUseruid);
-  const userDoc = await getDoc(userDocRef);
-  if (userDoc.exists()) {
-    const userData = userDoc.data();
-    const photoData = userData.skus
-      .flatMap((sku) =>
-        sku.photos.map((photo) => {
-          if (!photo.isDeleted) {
-            return null;
-          }
-          return {
-            url: photo.url,
-            date: photo.date,
-            sku: sku.sku,
-            title: sku.title,
-            description: sku.description,
-            quantity: sku.quantity,
-            price: sku.price,
-            type: sku.type,
-            isVideo: isVideo(photo.url),
-            isFavorite: photo.isFavorite,
-            allSearch: `${sku.sku} ${photo.title}`.toLowerCase(),
-          };
-        })
-      )
-      .filter((photo) => photo !== null);
-
-    const photosWithUrls = await Promise.all(
-      photoData.map(async (photo) => {
-        const storageRef = ref(storage, photo.url);
-        const downloadUrl = await getDownloadURL(storageRef);
-        return {
-          ...photo,
-          downloadUrl,
-        };
-      })
-    );
-
-    return photosWithUrls;
-  }
-};
 
 export const permentDelete = async (props) => {
   const { urls, currentUseruid, firestore, storage } = props;
   if (!urls || urls.length === 0) return;
 
-  const storageref = ref(storage, urls);
-  await deleteObject(storageref);
+  // Delete all files from storage
+  await Promise.all(
+    urls.map(async (url) => {
+      const storageref = ref(storage, url);
+      try {
+        await deleteObject(storageref);
+      } catch (error) {
+        console.error(`Error deleting file ${url}:`, error);
+      }
+    })
+  );
 
+  // Update Firestore document
   const userDocRef = doc(firestore, "Users", currentUseruid);
   const userDoc = await getDoc(userDocRef);
   if (userDoc.exists()) {
     const userData = userDoc.data();
-    const updatedSkus = userData.skus.map((sku) => ({
-      ...sku,
-      photos: sku.photos.filter((photo) => !urls.includes(photo.url)),
-    }));
+    const updatedSkus = userData.skus
+      .map((sku) => ({
+        ...sku,
+        photos: sku.photos.filter((photo) => !urls.includes(photo.url)),
+      }))
+      // Remove SKUs that have no photos
+      .filter((sku) => sku.photos.length > 0);
+
     await updateDoc(userDocRef, {
       skus: updatedSkus,
-    })
+    });
   }
 }
 
