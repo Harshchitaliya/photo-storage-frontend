8ee4af284403492ccd8508bf26c0ec5324190a74
Harshchitaliya <chitaliyaharsh09@gmail.com>
2024-11-05 15:56:55 +0530

    design a gallery module

--- src/components/FlowBiteTheme/index.jsx
+++ src/components/FlowBiteTheme/index.jsx
@@ -2,11 +2,13 @@
 import Card from "./Card";
 import Spinner from "./Spinner";
 import Modal from "./Modal";
+import Button from "./Button";
 
 const FlowbiteTheme = {
   ...Card,
   ...Spinner,
   ...Modal,
+  ...Button,
 };
 
 export default FlowbiteTheme;
