5551242f2d16770fb37a82a6feedc8e8b439bcb4
Harshchitaliya <chitaliyaharsh09@gmail.com>
2024-11-05 17:01:08 +0530

    fix code

--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -1,57 +1,57 @@
 import React from 'react';
 
 export const HomeIcon = (styles = {}) => (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
-        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-    </svg>
+  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
+    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+  </svg>
 );
 
 export const ProductsIcon = (styles = {}) => (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
-        <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd"></path>
-    </svg>
+  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
+    <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd"></path>
+  </svg>
 );
 
 export const GalleryIcon = (styles = {}) => (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
-        <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd"></path>
-    </svg>
+  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
+    <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd"></path>
+  </svg>
 );
 
 export const RecycleIcon = (styles = {}) => (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
-        <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd"></path>
-    </svg>
+  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
+    <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd"></path>
+  </svg>
 );
 
 export const CatalogIcon = (styles = {}) => (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
-        <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
-    </svg>
+  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
+    <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
+  </svg>
 );
 
 export const ProfileIcon = (styles = {}) => (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
-        <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
-    </svg>
+  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...styles}>
+    <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
+  </svg>
 );
 
 export const SidebarIcon = (styles = {}) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        className="w-4 h-4 text-gray-800"
-        {...styles}
-    >
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M15 19l-7-7 7-7"
-        />
-    </svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="w-4 h-4 text-gray-800"
+    {...styles}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M15 19l-7-7 7-7"
+    />
+  </svg>
 )
 
 export const LoginIcon = (styles = {}) => (
@@ -66,9 +66,9 @@ export const LogoutIcon = (styles = {}) => (
   </svg>
 );
 export const VideoIcon = (styles = {}) => (
-    <svg
+  <svg
     xmlns="http://www.w3.org/2000/svg"
-  
+
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -81,11 +81,11 @@ export const VideoIcon = (styles = {}) => (
     <polygon points="23 7 16 12 23 17 23 7"></polygon>
     <rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect>
   </svg>
-  
+
 );
 
 export const ImageIcon = (styles = {}) => (
-    <svg
+  <svg
     xmlns="http://www.w3.org/2000/svg"
     className="w-5 h-5"
     viewBox="0 0 24 24"
@@ -99,11 +99,11 @@ export const ImageIcon = (styles = {}) => (
     <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
     <circle cx="8.5" cy="8.5" r="1.5"></circle>
     <polyline points="21 15 16 10 5 21"></polyline>
-    </svg>
+  </svg>
 );
 
 export const MoreoptionsIcon = (styles = {}) => (
-    <svg
+  <svg
     className="w-5 h-5"
     fill="currentColor"
     viewBox="0 0 24 24"
@@ -173,45 +173,45 @@ export const ShareIcon = (styles = {}) => {
   );
 };
 export const SearchIcon = (styles = {}) => (
-  <svg 
-  className="w-6 h-6 text-gray-400"
-  fill="none" 
-  stroke="currentColor" 
-  viewBox="0 0 24 24"
-  {...styles}
->
-  <path 
-    strokeLinecap="round" 
-    strokeLinejoin="round" 
-    strokeWidth={2} 
-    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" 
-  />
-</svg>
+  <svg
+    className="w-6 h-6 text-gray-400"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    {...styles}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+    />
+  </svg>
 );
 export const FilterIcon = (styles = {}) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-5 h-5"
-      {...styles}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
-      />
-    </svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-5 h-5"
+    {...styles}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
+    />
+  </svg>
 );
 
 export const FavoriteIcon = (styles = {}) => (
-  <svg 
-    className="w-5 h-5" 
-    fill="currentColor" 
-    viewBox="0 0 24 24" 
-    xmlns="http://www.w3.org/2000/svg" 
+  <svg
+    className="w-5 h-5"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
     {...styles}
   >
     <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
@@ -307,5 +307,39 @@ export const SortDescIcon = (styles = {}) => (
       strokeWidth={2}
       d="M7 13l5 5 5-5"
     />
+  </svg>
+);
+
+// export const CancelIcon = (styles = {}) => (
+//   <svg 
+//     className="w-5 h-5" 
+//     viewBox="0 0 24 24" 
+//     fill="none" 
+//     stroke="currentColor"
+//     strokeWidth="2"
+//     strokeLinecap="round"
+//     strokeLinejoin="round"
+//     color="currentColor"
+//     {...styles}
+//   >
+//     <path d="M18 6L6 18" />
+//     <path d="M6 6l12 12" />
+//   </svg>
+// );
+
+export const CancelIcon = (styles = {}) => (
+  <svg
+    className="w-5 h-5"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...styles}
+  >
+    <circle cx="12" cy="12" r="10" />
+    <path d="M15 9l-6 6" />
+    <path d="M9 9l6 6" />
   </svg>
 );
--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { storage, firestore } from "../../context/auth/connection/connection";
 import { useAuth } from "../../context/auth/AuthContext";
 import ProductCard from "../../components/ProductCard";
-import { DeleteIcon, DownloadIcon, ShareIcon } from "../../components/Icons";
+import { DeleteIcon, DownloadIcon, ShareIcon, CancelIcon } from "../../components/Icons";
 import { Button, Checkbox, Toast } from "flowbite-react";
 import Loader from "../../components/Loader";
 import DrawerComponent from "./Drawer";
@@ -11,265 +11,270 @@ import { setAllPhoto, deletePhoto, setFavorite } from "../../server";
 import { ref, getDownloadURL } from "firebase/storage";
 
 const buttonList = [
-  { type: "all", label: "All", className: "rounded-r-none" },
-  { type: "image", label: "Image", className: "rounded-l-none" },
-  { type: "video", label: "Video" },
-  { type: "favorite", label: "Favorite" },
+    { type: "all", label: "All" },
+    { type: "image", label: "Image" },
+    { type: "video", label: "Video" },
+    { type: "favorite", label: "Favorite" },
 ];
 
 const Gallery = () => {
-  const [photo, setPhoto] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [search, setSearch] = useState("");
-  const [filteredPhoto, setFilteredPhoto] = useState([]);
-  const [type, setType] = useState("all");
-  const { currentUseruid } = useAuth();
-  const galleryphoto = true;
-  const isrecycle = false;
+    const [photo, setPhoto] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [selectedItems, setSelectedItems] = useState([]);
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const [search, setSearch] = useState("");
+    const [filteredPhoto, setFilteredPhoto] = useState([]);
+    const [type, setType] = useState("all");
+    const { currentUseruid } = useAuth();
+    const galleryphoto = true;
+    const isrecycle = false;
 
-  useEffect(() => {
-    let filtered = photo;
-    if (search) {
-      filtered = photo.filter((item) => item.allSearch.includes(search));
-    }
-    if (type !== "all") {
-      filtered = filtered.filter((item) =>
-        type === "image"
-          ? !item.isVideo
-          : type === "video"
-          ? item.isVideo
-          : item.isFavorite
-      );
-    }
-    setFilteredPhoto(filtered);
-  }, [search, photo, type]);
-
-  useEffect(() => {
-    if (drawerOpen) {
-      setSelectedItems([]);
-    }
-  }, [drawerOpen]);
-
-  useEffect(() => {
-    handleShowPhoto();
-  }, []);
-
-  const handleShowPhoto = async () => {
-    if (photo.length <= 0) {
-      // setLoading(true)
-    }
-
-    try {
-      const allphotos = await setAllPhoto({
-        currentUseruid,
-        firestore,
-        storage,
-        galleryphoto,
-      });
-      setPhoto(allphotos);
-    } catch (error) {
-      console.error("Error fetching photos:", error);
-    }
-    // } finally {
-    //     setLoading(false);
-    // }
-  };
-
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if ((e.ctrlKey || e.metaKey) && e.key === "a") {
-        e.preventDefault();
-        handleSelectAll();
-      }
-
-      if (e.keyCode === 27) {
-        e.preventDefault();
-        setSelectedItems([]);
-      }
-    };
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [photo]);
-
-  const handleSelectAll = (e) => {
-    if (e?.target?.type === "checkbox") {
-      if (e.target.checked) {
-        setSelectedItems(photo.map((item) => item.url));
-      } else {
-        setSelectedItems([]);
-      }
-    } else {
-      if (selectedItems.length === photo.length) {
-        setSelectedItems([]);
-      } else {
-        setSelectedItems(photo.map((item) => item.url));
-      }
-    }
-  };
-
-  const handleDelete = async (urls) => {
-    try {
-      setLoading(true);
-      await deletePhoto({ urls, currentUseruid, firestore, isrecycle });
-      await handleShowPhoto();
-      setSelectedItems([]);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleFavorite = async (urls) => {
-    try {
-      setLoading(true);
-      await setFavorite({
-        urls,
-        currentUseruid,
-        firestore,
-        handleShowPhoto,
-        setSelectedItems,
-      });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleDownload = async (urls) => {
-    try {
-      const urlArray = Array.isArray(urls) ? urls : [urls];
-
-      for (const url of urlArray) {
-        const storageRef = ref(storage, url);
-        try {
-          setLoading(true);
-          const downloadUrl = await getDownloadURL(storageRef);
-          const filename = downloadUrl.split("/").pop();
-          const atag = document.createElement("a");
-          atag.href = downloadUrl;
-          atag.setAttribute("download", filename);
-          document.body.appendChild(atag);
-          atag.click();
-          document.body.removeChild(atag);
-        } catch (error) {
-          console.error(`Error downloading ${url}:`, error);
+    useEffect(() => {
+        let filtered = photo;
+        if (search) {
+            filtered = photo.filter((item) => item.allSearch.includes(search));
         }
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+        if (type !== "all") {
+            filtered = filtered.filter((item) =>
+                type === "image"
+                    ? !item.isVideo
+                    : type === "video"
+                        ? item.isVideo
+                        : item.isFavorite
+            );
+        }
+        setFilteredPhoto(filtered);
+    }, [search, photo, type]);
 
-  const handleShare = async (url) => {
-    try {
-      console.log(url);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+    useEffect(() => {
+        if (drawerOpen) {
+            setSelectedItems([]);
+        }
+    }, [drawerOpen]);
 
-  return (
-    <div>
-      {loading && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
-          <Loader />
-        </div>
-      )}
+    useEffect(() => {
+        handleShowPhoto();
+    }, []);
 
-      <div className="flex items-center justify-between">
-        <div className="flex items-center p-2">
-          <Checkbox
-            className="cursor-pointer"
-            checked={
-              selectedItems.length === filteredPhoto.length &&
-              filteredPhoto.length > 0
+    const handleShowPhoto = async () => {
+        if (photo.length <= 0) {
+            // setLoading(true)
+        }
+
+        try {
+            const allphotos = await setAllPhoto({
+                currentUseruid,
+                firestore,
+                storage,
+                galleryphoto,
+            });
+            setPhoto(allphotos);
+        } catch (error) {
+            console.error("Error fetching photos:", error);
+        }
+        // } finally {
+        //     setLoading(false);
+        // }
+    };
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if ((e.ctrlKey || e.metaKey) && e.key === "a") {
+                e.preventDefault();
+                handleSelectAll();
             }
-            onChange={handleSelectAll}
-          />
-          <label
-            className="ml-2 text-sm text-gray-500 cursor-pointer"
-            onClick={handleSelectAll}
-          >
-            Select All
-          </label>
-        </div>
-        <Button.Group className="flex items-center">
-          {buttonList.map(({ type: buttonType, label, className }) => (
-            <Button
-              key={buttonType}
-              className={`${className} ${
-                type === buttonType ? "bg-gray-800" : ""
-              }`}
-              onClick={() => setType(buttonType)}
-            >
-              {label}
-            </Button>
-          ))}
-        </Button.Group>
-        <SearchInput onSearch={setSearch} />
-      </div>
 
-      <div
-        className="flex flex-wrap justify-center items-center gap-6 mt-3 overflow-y-auto"
-        style={{ height: "calc(100vh - 120px)" }}
-      >
-        {filteredPhoto.length > 0 ? (
-          filteredPhoto.map((photoUrl, index) => (
-            <ProductCard
-              photoUrl={photoUrl}
-              key={index}
-              checkboxClick={setSelectedItems}
-              checked={selectedItems}
-              handleDownload={() => handleDownload(photoUrl.downloadUrl)}
-              handleDelete={() => handleDelete(photoUrl.url)}
-              handleShare={() => handleShare(photoUrl.url)}
-              handleFavorite={() => handleFavorite(photoUrl.url)}
-              setDrawerOpen={setDrawerOpen}
-            />
-          ))
-        ) : (
-          <p>No photos available</p>
-        )}
-      </div>
+            if (e.keyCode === 27) {
+                e.preventDefault();
+                setSelectedItems([]);
+            }
+        };
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [photo]);
 
-      {selectedItems.length > 0 && (
-        <div className="fixed bottom-4 right-4">
-          <Toast>
-            <div className="flex items-center gap-4">
-              <span>{selectedItems.length} selected</span>
-              <Button onClick={() => handleDownload(selectedItems)}>
-                <DownloadIcon />
-              </Button>
-              <Button onClick={() => handleDelete(selectedItems)}>
-                <DeleteIcon />
-              </Button>
-              <Button onClick={() => handleShare(selectedItems)}>
-                <ShareIcon />
-              </Button>
+    const handleSelectAll = (e) => {
+        if (e?.target?.type === "checkbox") {
+            if (e.target.checked) {
+                setSelectedItems(photo.map((item) => item.url));
+            } else {
+                setSelectedItems([]);
+            }
+        } else {
+            if (selectedItems.length === photo.length) {
+                setSelectedItems([]);
+            } else {
+                setSelectedItems(photo.map((item) => item.url));
+            }
+        }
+    };
+
+    const handleDelete = async (urls) => {
+        try {
+            setLoading(true);
+            await deletePhoto({ urls, currentUseruid, firestore, isrecycle });
+            await handleShowPhoto();
+            setSelectedItems([]);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleFavorite = async (urls) => {
+        try {
+            setLoading(true);
+            await setFavorite({
+                urls,
+                currentUseruid,
+                firestore,
+                handleShowPhoto,
+                setSelectedItems,
+            });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleDownload = async (urls) => {
+        try {
+            const urlArray = Array.isArray(urls) ? urls : [urls];
+
+            for (const url of urlArray) {
+                const storageRef = ref(storage, url);
+                try {
+                    setLoading(true);
+                    const downloadUrl = await getDownloadURL(storageRef);
+                    const filename = downloadUrl.split("/").pop();
+                    const atag = document.createElement("a");
+                    atag.href = downloadUrl;
+                    atag.setAttribute("download", filename);
+                    document.body.appendChild(atag);
+                    atag.click();
+                    document.body.removeChild(atag);
+                } catch (error) {
+                    console.error(`Error downloading ${url}:`, error);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleShare = async (url) => {
+        try {
+            console.log(url);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div>
+            {loading && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+                    <Loader />
+                </div>
+            )}
+
+            <div className="flex items-center justify-between">
+                <div className="flex items-center p-2">
+                    <Checkbox
+                        className="cursor-pointer"
+                        checked={
+                            selectedItems.length === filteredPhoto.length &&
+                            filteredPhoto.length > 0
+                        }
+                        onChange={handleSelectAll}
+                    />
+                    <label
+                        className="ml-2 text-sm text-gray-500 cursor-pointer"
+                        onClick={handleSelectAll}
+                    >
+                        Select All
+                    </label>
+                </div>
+                <Button.Group className="flex items-center">
+                    {buttonList.map(({ type: buttonType, label, className }) => (
+                        <Button
+                            key={buttonType}
+
+                            className={` ${type === buttonType ? "dark:bg-gray-700" : ""
+                                }`}
+                            onClick={() => setType(buttonType)}
+                        >
+                            {label}
+                        </Button>
+                    ))}
+                </Button.Group>
+                <SearchInput onSearch={setSearch} />
             </div>
-          </Toast>
+
+            <div
+                className="flex flex-wrap justify-center items-center gap-6 mt-3 overflow-y-auto"
+                style={{ height: "calc(100vh - 120px)" }}
+            >
+                {filteredPhoto.length > 0 ? (
+                    filteredPhoto.map((photoUrl, index) => (
+                        <ProductCard
+                            photoUrl={photoUrl}
+                            key={index}
+                            checkboxClick={setSelectedItems}
+                            checked={selectedItems}
+                            handleDelete={handleDelete}
+                            handleShare={handleShare}
+                            setDrawerOpen={setDrawerOpen}
+                            handleFavorite={handleFavorite}
+                            handleDownload={handleDownload}
+                        />
+                    ))
+                ) : (
+                    <p>No photos available</p>
+                )}
+            </div>
+
+            {selectedItems.length > 0 && (
+                <div className="fixed bottom-4 right-4">
+                    <Toast>
+                        <div className="flex items-center gap-4">
+                            <span>{selectedItems.length} selected</span>
+                            <Button size="sm" onClick={() => handleDownload(selectedItems)}>
+                                <DownloadIcon />
+                            </Button>
+                            <Button size="sm" onClick={() => handleShare(selectedItems)}>
+                                <ShareIcon />
+                            </Button>
+                            <Button size="sm" color="failure" onClick={() => handleDelete(selectedItems)}>
+                                <DeleteIcon />
+                            </Button>
+                        
+                        <Button size="sm" onClick={() => setSelectedItems([])}>
+                            <CancelIcon />
+                        </Button>
+
+                        </div>
+                    </Toast>
+                </div>
+            )}
+            {drawerOpen && (
+                <DrawerComponent
+                    drawerOpen={drawerOpen}
+                    setDrawerOpen={setDrawerOpen}
+                    handleDownload={handleDownload}
+                    handleDelete={handleDelete}
+                    handleShare={handleShare}
+                />
+            )}
         </div>
-      )}
-      {drawerOpen && (
-        <DrawerComponent
-          drawerOpen={drawerOpen}
-          setDrawerOpen={setDrawerOpen}
-          handleDownload={handleDownload}
-          handleDelete={handleDelete}
-          handleShare={handleShare}
-        />
-      )}
-    </div>
-  );
+    );
 };
 export default Gallery;
