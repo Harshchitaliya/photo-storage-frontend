ec06d3270ce5c5d112660ab3a47df3c3eeae79c6
Harshchitaliya <chitaliyaharsh09@gmail.com>
2024-11-06 19:38:20 +0530

    working on

--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -326,16 +326,6 @@ export const CancelIcon = (styles = {}) => (
     <path d="M9 9l6 6" />
   </svg>
 );
-export const BackIcon = (styles = {}) => (
-  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" {...styles}>
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-  </svg>
-);
-export const RemoveBackgroundIcon = (styles = {}) => (
-  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" {...styles}>
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
-  </svg>
-);
 
 export const ZoomInIcon = (styles = {}) => (
   <svg 
