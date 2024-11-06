8484145b09b8160ae840a3a120df476516c7844a
Harshchitaliya <chitaliyaharsh09@gmail.com>
2024-11-06 19:46:29 +0530

    Merge remote-tracking branch 'origin/harsh'

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,17 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import { useAuth } from '../context/auth/AuthContext.jsx';
-import Gallery from '../pages/Gallery';
-import Upload from '../pages/Gallery/upload';
-import Product from '../pages/Product';
-import Loader from '../components/Loader';
-import EditProduct from '../pages/EditProduct';
-import Recycle from '../pages/Recycle';
-import Profile from '../pages/Profile';
-import EditImg from '../pages/EditImg';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import { useAuth } from "../context/auth/AuthContext.jsx";
+import Gallery from "../pages/Gallery";
+import Upload from "../pages/Gallery/upload";
+import Product from "../pages/Product";
+import Loader from "../components/Loader";
+import EditProduct from "../pages/EditProduct";
+import Recycle from "../pages/Recycle";
+import Profile from "../pages/Profile";
+import EditImg from "../pages/EditImg";
 function AppRoutes() {
-  const { loading , currentUseruid } = useAuth();
+  const { loading, currentUseruid } = useAuth();
   // useEffect(() => {
   //   const handleKeyDown = (e) => {
   //     if(e.keyCode === 8){
@@ -26,7 +26,7 @@ function AppRoutes() {
   if (loading) {
     return (
       <div className="flex items-center justify-center min-h-screen">
-        <Loader/>
+        <Loader />
       </div>
     );
   }
@@ -34,14 +34,36 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={!currentUseruid ? <Login /> : <Navigate to="/" />} />
-      <Route path="/gallery" element={currentUseruid ? <Gallery /> : <Navigate to="/login" />} />
-      <Route path="/upload" element={currentUseruid ? <Upload /> : <Navigate to="/login" />} />
-      <Route path="/products" element={currentUseruid ? <Product /> : <Navigate to="/login" />} />
-      <Route path="/products/:id/edit" element={currentUseruid ? <EditProduct /> : <Navigate to="/login" />} />
-      <Route path="/recycle" element={currentUseruid ? <Recycle /> : <Navigate to="/login" />} />
-      <Route path="/profile" element={currentUseruid ? <Profile /> : <Navigate to="/login" />} />
-      <Route path="/products/:id/:imgId/edit" element={currentUseruid ? <EditImg /> : <Navigate to="/login" />} />
+      <Route
+        path="/login"
+        element={!currentUseruid ? <Login /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/gallery"
+        element={currentUseruid ? <Gallery /> : <Login />}
+      />
+      <Route path="/upload" element={currentUseruid ? <Upload /> : <Login />} />
+      <Route
+        path="/products"
+        element={currentUseruid ? <Product /> : <Login />}
+      />
+      <Route
+        path="/products/:id/edit"
+        element={currentUseruid ? <EditProduct /> : <Login />}
+      />
+      <Route
+        path="/products/:id/:imgId/edit"
+        element={currentUseruid ? <EditImg /> : <Login />}
+      />
+      <Route
+        path="/recycle"
+        element={currentUseruid ? <Recycle /> : <Login />}
+      />
+      <Route
+        path="/profile"
+        element={currentUseruid ? <Profile /> : <Login />}
+      />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
