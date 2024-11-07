77ee60214cd9c0d80a3bd2218aa6fdcd86d6692f
Harshchitaliya <chitaliyaharsh09@gmail.com>
2024-11-07 10:36:09 +0530

    Merge remote-tracking branch 'origin/harsh'

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./assets/styles/App.css";
 import { AuthProvider } from "./context/auth/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
-import Layout from './components/Layout/Layout.jsx';
+import Layout from './components/layout';
 import { ThemeProvider } from './utils/ThemeProvider.jsx';
 
 const App = () => {
